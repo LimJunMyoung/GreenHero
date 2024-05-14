f0aa90af1f7b7b4d5b3a3f5e296ed96c7c338ee4
LimJunMyoung <64548186+LimJunMyoung@users.noreply.github.com>
2024-05-14 12:44:43 +0900

    update

---
+++ Light/전등/light.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const switchElement = document.querySelector('.switch');
+    const contElement_1 = document.querySelector('.opacity');
+    //const contElement_2 = document.querySelector('.curtain');
+    const imgElement = switchElement.querySelector('img');
+
+    let isToggled = false;
+
+    switchElement.addEventListener('click', function() {
+        if (isToggled) {
+            contElement_1.style.opacity = '0';
+            //contElement_2.style.opacity = '0';
+            imgElement.src = './img/button_on.svg';
+        } else {
+            contElement_1.style.opacity = '0.2';
+            //contElement_2.style.opacity = '0.9';
+            imgElement.src = './img/button_off.svg';
+        }
+        isToggled = !isToggled;
+    });
+});
+
+
+
---
+++ Light/커튼/curtain.js
@@ -0,0 +1,44 @@
+// div 요소 가져오기
+var resizableDiv = document.getElementById('resizableDiv');
+
+// 마우스 다운 이벤트 처리
+resizableDiv.addEventListener('mousedown', function(event) {
+    // 시작 좌표 설정
+    var startX = event.clientX;
+    var startY = event.clientY;
+
+    // div의 초기 크기
+    var startWidth = parseInt(document.defaultView.getComputedStyle(resizableDiv).width, 10);
+    var startHeight = parseInt(document.defaultView.getComputedStyle(resizableDiv).height, 10);
+
+    // 마우스 이동 이벤트 처리
+    function onMouseMove(event) {
+        // 이동한 거리 계산
+        var deltaX = event.clientX - startX;
+        var deltaY = event.clientY - startY;
+
+        // 새로운 너비 계산
+        var newWidth = startWidth + deltaX;
+
+        // 최소 및 최대 너비 설정
+        var minWidth = 112; // 최소 너비
+        var maxWidth = 448; // 최대 너비
+
+        // 너비가 최소값보다 작으면 최소값으로, 최대값보다 크면 최대값으로 설정
+        newWidth = Math.max(Math.min(newWidth, maxWidth), minWidth);
+
+        // div의 새 너비 설정
+        resizableDiv.style.width = newWidth + 'px';
+    }
+
+    // 마우스 업 이벤트 처리
+    function onMouseUp(event) {
+        // 이동 및 업 이벤트 리스너 제거
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+    }
+
+    // 이벤트 리스너 추가
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+});
